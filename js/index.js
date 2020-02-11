80dc98866e379b98aabd189ea0524d84b8c19d12
unknown <william.z.wilson95@gmail.com>
2020-02-11 14:39:06 -0600

    did it again

--- js/index.js
+++ js/index.js
@@ -1,85 +1,59 @@
-nav = document.querySelector('.nav')
+nav = document.querySelector(".nav");
 
-nav.addEventListener("mouseenter", () => {      ///1
+nav.addEventListener("mouseleave", () => {
+  nav.style.transform = "scale(1)";
 
-nav.style.transform = "scale(1.2)"              
+  nav.style.transition = "transform 1s";
+});
 
-nav.style.transition = "transform 1.2s"
-})
+nav.addEventListener("mouseenter", () => {
+  nav.style.transform = "scale(1)";
+  nav.style.transition = "transform 1s";
+});
 
-nav.addEventListener('mouseleave', () => { //2
-    nav.style.transform = "scale(1)"
+gone = document.querySelector(".content-section");
+gone.addEventListener("wheel", () => {
+  gone.style.backgroundColor = "grey";
+});
 
-nav.style.transition = "transform 1.2s"
-})
+img = document.querySelector("img");
+img.addEventListener("click", event => {
+  img.style.display = "none";
+  event.stopPropagation();
+});
 
+pink = document.querySelector(".footer");
+pink.addEventListener("dblclick", () => {
+  pink.style.backgroundColor = "purple";
+});
 
-words = document.querySelector('.intro')
-words.addEventListener('click', () => {         //3
-    words.style.backgroundColor = 'blue'
-})
+words = document.querySelector(".intro");
+words.addEventListener("click", () => {
+  words.style.backgroundColor = "green";
+});
 
-img = document.querySelector('img')
-img.addEventListener('click', (event) => {      //3 DOES NOT BUBBLE
-    img.style.display = 'none'
-    event.stopPropagation()
-})
+orange = document.querySelector(".btn");
+orange.addEventListener("mousedown", () => {
+  orange.style.display = "none";
+});
 
+copy = document.querySelector(".destination");
+copy.addEventListener("copy", () => {
+  copy.style.backgroundColor = "orange";
+});
 
-gone = document.querySelector('.content-section') //4
-gone.addEventListener('wheel', () => {         
-    gone.style.backgroundColor = 'yellow'
-})
+pick = document.querySelector(".content-pick");
+pick.addEventListener("contextmenu", () => {
+  pick.style.backgroundColor = "purple";
+});
 
-pink = document.querySelector('.footer') //5
-pink.addEventListener('dblclick', () => {         
-    pink.style.backgroundColor = 'pink'
-})
+blue = document.querySelector("body");
+blue.addEventListener("mouseup", () => {
+  blue.style.backgroundColor = "green";
+});
 
-orange = document.querySelector('.btn')
-orange.addEventListener('mousedown', () => {        //6  
-    orange.style.display = 'none'
-})
-
-
-copy = document.querySelector('.destination')   //7
-copy.addEventListener('copy', () => {         
-    copy.style.backgroundColor = 'green'
-})
-
-pick = document.querySelector('.content-pick')   //8
-pick.addEventListener('contextmenu', () => {         
-    pick.style.backgroundColor = 'purple'
-})
-
-
-blue = document.querySelector('body')   //9
-blue.addEventListener('mouseup', () => {         
-    blue.style.backgroundColor = 'blue'
-})
-
-
-document.querySelectorAll('.nav-link').forEach(item=>{ //10
-    item.addEventListener('mousemove' , (event) => {
-        event.preventDefault()
-    })
-})     
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+document.querySelectorAll(".nav-link").forEach(item => {
+  item.addEventListener("mousemove", event => {
+    event.preventDefault();
+  });
+});
